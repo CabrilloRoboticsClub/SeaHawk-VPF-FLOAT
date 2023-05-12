9a9a4be323a1f00a149198799623de7cce783e95
cTurtle98 <ciaran@cturtle98.com>
2023-05-12 14:21:44 -0700

    remove gps

--- code.py
+++ code.py
@@ -34,33 +34,16 @@ import adafruit_rfm9x
 
 import time
 
-# gps wing library
-import adafruit_gps
-
-# get board details
-board_type = os.uname().machine
-
 # instantiate the spi interface
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# instanciate uart interface
-uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 
 
 #
 # LoRa Radio Wing SETUP
 #
 
-# set the Chip Select and Reset pins based on board type
-if "Particle Xenon" in board_type:
-    CS = digitalio.DigitalInOut(board.D2)
-    RESET = digitalio.DigitalInOut(board.D3)
-elif "RFM9x" in board_type:
-    CS = digitalio.DigitalInOut(board.RFM9X_CS)
-    RESET = digitalio.DigitalInOut(board.RFM9X_RST)
-else:
-    CS = digitalio.DigitalInOut(board.D5)
-    RESET = digitalio.DigitalInOut(board.D6)
+CS = digitalio.DigitalInOut(board.D5)
+RESET = digitalio.DigitalInOut(board.D6)
 
 # set the radio frequency to 915mhz
 RADIO_FREQ_MHZ = 915.0 
@@ -74,21 +57,6 @@ rfm9x.node = 100
 # set the destination lora node ID
 # destination is deck
 rfm9x.destination = 50
-
-#
-# gps wing setup
-#
-
-# instanciate the gps object
-gps = adafruit_gps.GPS(uart, debug=False)
-
-# enable all features
-gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
-
-# Set update rate to every half second
-gps.send_command(b"PMTK220,500")
-
-
 
 while True:
 
